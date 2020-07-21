24bf68e866523b6b22c03b0b833b63e79e4fb0ff
Parth Sharma <parthsharma151@gmail.com>
2020-07-21 10:24:53 +0530

    added button scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() => runApp(new FitnessCalculator());
 class FitnessCalculator extends StatelessWidget {
   Widget build(BuildContext buildContext) {
     return new MaterialApp(
-      title: null,
+      title: 'Fitness Calculator',
       home: new FitnessCalc(),
     );
   }
@@ -17,8 +17,77 @@ class FitnessCalc extends StatefulWidget {
 
 class FitnessCalcState extends State<FitnessCalc> {
   Widget build(BuildContext buildContext) {
+    Widget btn(btnText, Color color) {
+      return Container(
+        child: new RaisedButton(
+          child: Text(btnText),
+          onPressed: () {},
+          color: color,
+        ),
+      );
+    }
+
     return new Scaffold(
       appBar: null,
+      backgroundColor: Colors.black,
+      body: new Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  '123',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 60.0,
+                    // fontFamily:
+                  ),
+                  textAlign: TextAlign.right,
+                ),
+              )
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              btn('C', const Color(0xffa5a5a5)),
+              btn('+/-', const Color(0xffa5a5a5)),
+              btn('%', const Color(0xffa5a5a5)),
+              btn('÷', Colors.orange),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              btn('7', const Color(0xffa5a5a5)),
+              btn('8', const Color(0xffa5a5a5)),
+              btn('9', const Color(0xffa5a5a5)),
+              btn('x', Colors.orange),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              btn('4', const Color(0xffa5a5a5)),
+              btn('5', const Color(0xffa5a5a5)),
+              btn('6', const Color(0xffa5a5a5)),
+              btn('-', Colors.orange),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              btn('1', const Color(0xffa5a5a5)),
+              btn('2', const Color(0xffa5a5a5)),
+              btn('3', const Color(0xffa5a5a5)),
+              btn('+', Colors.orange),
+            ],
+          ),
+          Row(
+            children: <Widget>[
+              btn('0', const Color(0xffa5a5a5)),
+              btn('.', const Color(0xffa5a5a5)),
+              btn('=', Colors.orange),
+            ],
+          ),
+        ],
+      ),
     );
-  }
+  } // end of state class
 }
