72e40f55478f126c36d68d2dfc9ed40d916a7a95
Parth Sharma <parthsharma151@gmail.com>
2020-07-21 10:43:49 +0530

    fine tuned buttons

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,19 @@ class FitnessCalcState extends State<FitnessCalc> {
   Widget build(BuildContext buildContext) {
     Widget btn(btnText, Color color) {
       return Container(
+        padding: EdgeInsets.only(bottom: 10.0, top: 10.0),
         child: new RaisedButton(
-          child: Text(btnText),
+          child: Text(
+            btnText,
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+            ),
+          ),
           onPressed: () {},
           color: color,
+          padding: EdgeInsets.all(20.0),
+          shape: CircleBorder(),
         ),
       );
     }
@@ -31,6 +40,7 @@ class FitnessCalcState extends State<FitnessCalc> {
       appBar: null,
       backgroundColor: Colors.black,
       body: new Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Row(
             children: <Widget>[
@@ -48,6 +58,7 @@ class FitnessCalcState extends State<FitnessCalc> {
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
               btn('C', const Color(0xffa5a5a5)),
               btn('+/-', const Color(0xffa5a5a5)),
@@ -56,33 +67,38 @@ class FitnessCalcState extends State<FitnessCalc> {
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              btn('7', const Color(0xffa5a5a5)),
-              btn('8', const Color(0xffa5a5a5)),
-              btn('9', const Color(0xffa5a5a5)),
-              btn('x', Colors.orange),
+              btn('7', const Color(0xff333333)),
+              btn('8', const Color(0xff333333)),
+              btn('9', const Color(0xff333333)),
+              btn('X', Colors.orange),
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              btn('4', const Color(0xffa5a5a5)),
-              btn('5', const Color(0xffa5a5a5)),
-              btn('6', const Color(0xffa5a5a5)),
+              btn('4', const Color(0xff333333)),
+              btn('5', const Color(0xff333333)),
+              btn('6', const Color(0xff333333)),
               btn('-', Colors.orange),
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              btn('1', const Color(0xffa5a5a5)),
-              btn('2', const Color(0xffa5a5a5)),
-              btn('3', const Color(0xffa5a5a5)),
+              btn('1', const Color(0xff333333)),
+              btn('2', const Color(0xff333333)),
+              btn('3', const Color(0xff333333)),
               btn('+', Colors.orange),
             ],
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              btn('0', const Color(0xffa5a5a5)),
-              btn('.', const Color(0xffa5a5a5)),
+              btn('0', const Color(0xff333333)),
+              btn('0', const Color(0xff333333)),
+              btn('.', const Color(0xff333333)),
               btn('=', Colors.orange),
             ],
           ),
